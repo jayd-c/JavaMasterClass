d418d824eebb98ed4c2f1f517dd531abac2c5985
JayanDesilva <jaycdesilva@gmail.com>
2024-01-24 13:10:39 -0500

    Encapsulation cont..

--- Encapsulation/src/EnhancedPlayer.java
+++ Encapsulation/src/EnhancedPlayer.java
@@ -1,15 +1,15 @@
 public class EnhancedPlayer {
-    private String name;
+    private String fullName;
     private int health;
     private String weapon;
 
-    public EnhancedPlayer(String name) {
-        this(name, 100, "Sword");
-        this.name = name;
+    public EnhancedPlayer(String fullName) {
+        this(fullName, 100, "Sword");
+        this.fullName = fullName;
     }
 
-    public EnhancedPlayer(String name, int health, String weapon) {
-        this.name = name;
+    public EnhancedPlayer(String fullName, int health, String weapon) {
+        this.fullName = fullName;
         if(health <=0) this.health = 1;
         else if (health > 100) this.health = 100;
         else this.health = health;
