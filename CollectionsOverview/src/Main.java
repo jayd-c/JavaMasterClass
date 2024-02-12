51d3483364f239805995d046b69904a6d61fbd44
JayanDesilva <jaycdesilva@gmail.com>
2024-02-12 01:55:22 -0500

    Collections Overview

--- CollectionsOverview/src/Main.java
+++ CollectionsOverview/src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Collection<String> list = new HashSet<>();
+        String[] names = {"Anna","Bob","Carol","David","Edna"};
+        list.addAll(Arrays.asList(names));
+        System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("Greg","Gary","Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list : ?" + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Gary is in the list ? "+ list.contains("Gary"));
+
+
+
+    }
 }
